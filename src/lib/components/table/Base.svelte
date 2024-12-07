d0bccc12ac39e7b7e8e7c58749569c64aa91819b
Tamás Vince Kornél <vince@tvk.lol>
2024-12-07 18:17:41 +0100

    refactor: replace RoomSubstitutionTable and SubstitutionTable with reusable Base component for improved maintainability

---
+++ src/lib/components/table/Base.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+  import dayjs from 'dayjs';
+  import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+  import huLocale from 'dayjs/locale/hu';
+
+  dayjs.locale(huLocale);
+  dayjs.extend(LocalizedFormat)
+
+  let { data, date, tableHeaders, title, children } = $props();
+</script>
+
+<div class="overflow-hidden rounded-xl shadow-xl">
+  <div
+    class="flex items-center justify-between border-b-2 border-petrik-3 bg-black bg-opacity-30 px-2 md:px-4 py-2"
+  >
+    <div class="flex gap-0.5 md:gap-2">
+      <h1 class="font-bold md:text-2xl">{title}</h1>
+      <p class="font-light hidden self-end md:block md:text-lg">{data.length} db</p>
+    </div>
+    <h2 class="hidden md:block text-2xl font-semibold">{dayjs(date).format("LL")} - {dayjs(date).format("dddd")}</h2>
+    <h2 class="block md:hidden">{dayjs(date).format("L")} - {dayjs(date).format("dddd")}</h2>
+  </div>
+  <table class="min-w-full">
+    <thead>
+      <tr class="border-b border-petrik-3 bg-black bg-opacity-20">
+        {#each tableHeaders as header}
+          <th class="px-2 md:px-4 md:py-2 py-1 text-left font-semibold" class:hide-mobile={header.hideMobile === true}>
+            <div class="flex items-center gap-0.5 md:gap-2">
+              <Icon icon={header.icon} class="h-4 w-4 md:h-6 md:w-6" />
+              <span class="text-sm md:text-lg">{header.name}</span>
+            </div>
+          </th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {@render children()}
+    </tbody>
+  </table>
+</div>
+
+<style lang="postcss">
+  th.hide-mobile {
+    @apply hidden md:table-cell;
+  }
+</style>
---
+++ src/lib/components/table/RoomSub.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Base from "./Base.svelte";
+
+  let { roomSubstitutions, date } = $props()
+  
+  const tableHeaders = [
+    { name: '', icon: 'akar-icons:clock' },
+    { name: 'Honnan', icon: 'lucide:arrow-up-from-line' },
+    { name: 'Hova', icon: 'lucide:arrow-down-from-line' },
+    { name: 'Osztály', icon: 'fluent:people-team-16-filled' }
+  ];
+</script>
+
+<Base data={roomSubstitutions} {date} {tableHeaders} title="Teremcserék">
+  {#each roomSubstitutions as row, index}
+    {@const odd = index % 2 == 0}
+    <tr class="bg-black" class:bg-opacity-30={odd} class:bg-opacity-20={!odd}>
+      <td class="font-bold ml-1!">{row.lesson}.</td>
+      <td>{row.fromRoom.short}</td>
+      <td>{row.toRoom.short}</td>
+      <td>{row.class.name}</td>
+    </tr>
+  {/each}
+</Base>
+
+<style lang="postcss">
+  tr:last-child td {
+    border-bottom-width: 0;
+  }
+
+  tr td {
+    @apply text-sm md:text-base px-2 md:px-4 py-1 md:py-2 border-b border-petrik-3;
+  }
+</style>
