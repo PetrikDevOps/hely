631237c9f26238dd800dad76cf032b8de65d4bc3
Tamás Vince Kornél <vince@tvk.lol>
2024-12-07 22:23:59 +0100

    feat: add date parsing utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,18 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import huLocale from 'dayjs/locale/hu';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale(huLocale);
+
 export const capitalize = (s: string) => {
   return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export function parseDate (date: string): dayjs.Dayjs {
+  const parsed = dayjs.tz(date, 'UTC');
+  parsed.tz('Europe/Budapest');
+  return parsed;
 }
