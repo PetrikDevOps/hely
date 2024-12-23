624b5cdd3ec3a300739f76c27a047eed6e78b946
Tamás Vince Kornél <vince@tvk.lol>
2024-12-23 16:49:58 +0100

    feat: switch to singleton dayjs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,23 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import huLocale from 'dayjs/locale/hu';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
 dayjs.locale(huLocale);
+
+export { dayjs };
 
 export const capitalize = (s: string) => {
   return s.charAt(0).toUpperCase() + s.slice(1);
-}
+};
 
-export function parseDate (date: string): dayjs.Dayjs {
+export function parseDate(date: string): dayjs.Dayjs {
   const parsed = dayjs.tz(date, 'UTC');
   parsed.tz('Europe/Budapest');
   return parsed;
